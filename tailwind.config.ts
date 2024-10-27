55e4dfbdb853fe18cf9180d444cffe674f0a317b
maxwaselk <69905880+maxwaselk@users.noreply.github.com>
2024-10-27 19:56:30 +0100

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,19 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 }
+
 export default config
